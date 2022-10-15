89c6783197cde18af7653fbab2f12cbf8baaf329
Yeeun_An <48678872+yeeSilver@users.noreply.github.com>
2022-10-16 00:55:12 +0900

    Revert "add : createTodoPage 추가"

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import Button from "./components/Button";
-import CreateTodo from "./pages/todoPage/createTodoPage";
 
 function App() {
   return (
-    <>
-      <div className="App">
-        안녕하세요 화이팅 해봐요. 설정 잘 됐을라나.. <br />
-        절대경로 설정해야하는뎁..
-        <Button />
-      </div>
-      <CreateTodo />
-    </>
+    <div className="App">
+      안녕하세요 화이팅 해봐요. 설정 잘 됐을라나.. <br />
+      절대경로 설정해야하는뎁..
+      <Button />
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Router from "./Router";
-import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <Router />
-    </RecoilRoot>
+    <App />
   </React.StrictMode>
 );
 
