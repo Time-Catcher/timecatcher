4cf680fb0d0f75faa0cb90a9e9e7d37ac5e90796
wonoh <cwo1401@gmail.com>
2022-10-16 04:20:51 +0900

    modify: Auth component UI complete

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,10 +1,10 @@
-import styled from "styled-components";
 import { useForm, SubmitHandler } from "react-hook-form";
 import React from "react";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../../firebaseConfig";
 import { Link } from "react-router-dom";
 import {
+  Global,
   AuthContainer,
   AuthForm,
   AuthInputBox,
@@ -42,71 +42,75 @@ export default function SignUp() {
   };
 
   return (
-    <AuthContainer>
-      <h2>SIGNUP</h2>
-      <AuthForm onSubmit={handleSubmit(onSubmit)}>
-        <AuthInputBox>
-          <label htmlFor="email">이메일</label>
-          <input
-            type="text"
-            {...register("email", {
-              required: "필수 입력값입니다.",
-              validate: (email) =>
-                (email.includes("@") && email.includes(".")) ||
-                "이메일 형식을 지켜주세요.",
-            })}
-          />
-        </AuthInputBox>
-        {errors.email && <ValidateError>{errors.email?.message}</ValidateError>}
-        <AuthInputBox>
-          <label htmlFor="nickname">별명</label>
-          <input
-            {...register("nickname", {
-              required: "필수 입력값입니다.",
-              validate: (nickname) =>
-                nickname.trim().length >= 3 || "닉네임 형식을 지켜주세요.",
-            })}
-          />
-        </AuthInputBox>
-        {errors.nickname && (
-          <ValidateError>{errors.nickname?.message}</ValidateError>
-        )}
-        <AuthInputBox>
-          <label htmlFor="password">비밀번호</label>
-          <input
-            type="password"
-            {...register("password", {
-              required: "필수 입력값입니다.",
-              validate: (password) =>
-                password.length >= 8 || "패스워드 형식을 지켜주세요.",
-            })}
-          />
-        </AuthInputBox>
-        {errors.password && (
-          <ValidateError>{errors.password?.message}</ValidateError>
-        )}
-        <AuthInputBox>
-          <label htmlFor="passwordCheck">비밀번호 확인</label>
-          <input
-            type="password"
-            {...register("passwordCheck", {
-              required: true,
-              validate: (passwordCheck) =>
-                passwordCheck === watch("password") ||
-                "비밀번호가 일치하지 않습니다.",
-            })}
-          />
-        </AuthInputBox>
-        {errors.passwordCheck && (
-          <ValidateError>{errors.passwordCheck?.message}</ValidateError>
-        )}
-        <SubmitBtn>
-          <span>회원가입</span>
-        </SubmitBtn>
-      </AuthForm>
-      <ToggleAuth>
-        <span>로그인 하러가기</span> <Link to="/">로그인</Link>
-      </ToggleAuth>
-    </AuthContainer>
+    <Global>
+      <AuthContainer>
+        <h2>SIGNUP</h2>
+        <AuthForm onSubmit={handleSubmit(onSubmit)}>
+          <AuthInputBox>
+            <label htmlFor="email">이메일</label>
+            <input
+              type="text"
+              {...register("email", {
+                required: "필수 입력값입니다.",
+                validate: (email) =>
+                  (email.includes("@") && email.includes(".")) ||
+                  "이메일 형식을 지켜주세요.",
+              })}
+            />
+          </AuthInputBox>
+          {errors.email && (
+            <ValidateError>{errors.email?.message}</ValidateError>
+          )}
+          <AuthInputBox>
+            <label htmlFor="nickname">별명</label>
+            <input
+              {...register("nickname", {
+                required: "필수 입력값입니다.",
+                validate: (nickname) =>
+                  nickname.trim().length >= 3 || "닉네임 형식을 지켜주세요.",
+              })}
+            />
+          </AuthInputBox>
+          {errors.nickname && (
+            <ValidateError>{errors.nickname?.message}</ValidateError>
+          )}
+          <AuthInputBox>
+            <label htmlFor="password">비밀번호</label>
+            <input
+              type="password"
+              {...register("password", {
+                required: "필수 입력값입니다.",
+                validate: (password) =>
+                  password.length >= 8 || "패스워드 형식을 지켜주세요.",
+              })}
+            />
+          </AuthInputBox>
+          {errors.password && (
+            <ValidateError>{errors.password?.message}</ValidateError>
+          )}
+          <AuthInputBox>
+            <label htmlFor="passwordCheck">비밀번호 확인</label>
+            <input
+              type="password"
+              {...register("passwordCheck", {
+                required: true,
+                validate: (passwordCheck) =>
+                  passwordCheck === watch("password") ||
+                  "비밀번호가 일치하지 않습니다.",
+              })}
+            />
+          </AuthInputBox>
+          {errors.passwordCheck && (
+            <ValidateError>{errors.passwordCheck?.message}</ValidateError>
+          )}
+          <SubmitBtn>
+            <span>회원가입</span>
+          </SubmitBtn>
+        </AuthForm>
+        <ToggleAuth>
+          <span>로그인 하러가기</span> <Link to="/">로그인</Link>
+        </ToggleAuth>
+      </AuthContainer>
+    </Global>
   );
 }
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -3,6 +3,8 @@ import SignIn from "../components/auth/SignIn";
 import { getAuth } from "firebase/auth";
 import { useNavigate } from "react-router";
 import { onAuthStateChanged } from "firebase/auth";
+import styled from "styled-components";
+// import WizardCat from '../../public/images/wizardCat.png'
 
 export default function SignInPage() {
   const navigate = useNavigate();
@@ -16,5 +18,41 @@ export default function SignInPage() {
   //   });
   // }, []);
 
-  return <SignIn />;
+  return (
+    <AuthContainer>
+      <AuthLogo>
+        <img src="images/wizardCat.png" alt="위자드캣 logo" />
+        <h1>Cat`cher</h1>
+      </AuthLogo>
+      <SignIn />
+    </AuthContainer>
+  );
 }
+
+const AuthContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #6750a4;
+  height: 100vh;
+
+  & h2 {
+    font-size: 20px;
+  }
+`;
+
+const AuthLogo = styled.div`
+  & img {
+    width: 150px;
+  }
+
+  & h1 {
+    font-size: 32px;
+    margin-top: 4px;
+    color: #fff;
+    text-align: center;
+  }
+`;
+
+export { AuthContainer, AuthLogo };
